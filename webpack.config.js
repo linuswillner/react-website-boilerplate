27575e12f7831cfb5c50f0650fbb9a345230fd29
Linus Willner <hello@linuswillner.me>
2021-01-04 20:30:56 +0200

    Re-add accidentally removed progress plugin

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,8 @@ const EnvironmentConfig = new webpack.DefinePlugin({
 const sharedPlugins = [
   HTMLInjecterConfig,
   CSSExtracterConfig,
-  new NodePolyfillWebpackPlugin()
+  new NodePolyfillWebpackPlugin(),
+  new webpack.ProgressPlugin()
 ]
 
 const devPlugins = [
